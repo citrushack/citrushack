c1cb8946d6f28e29252147ca4a617e54feeed337
omoman <mannysanchez998@gmail.com>
2015-01-10 21:29:18 -0800

    Finished up faq section

--- site/js/src/controllers/mainCtrl.js
+++ site/js/src/controllers/mainCtrl.js
@@ -8,6 +8,10 @@
     function mainCtrl ($scope) {
         // Name of the view
         $scope.pageView = 'main-view';
+        // Security breach elaboration
+        $scope.securityElaborate = 'Although your project could be security related';
+        // Project ideas explanation
+        $scope.projectIdeasType = 'These projects would be a mix of humanitarian and utility applications'; 
     }
 
     /**
