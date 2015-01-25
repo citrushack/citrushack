994ae525d70d00d5e9776070944cd74b2dc151ea
omoman <mannysanchez998@gmail.com>
2015-01-25 10:54:41 -0800

    Remove ch-scroll-to and now using du scrollto
    Also started nav links

--- site/js/src/app.js
+++ site/js/src/app.js
@@ -5,12 +5,14 @@
      * Create main app
      */
      
-    angular.module('CitrusHack', ['ngAnimate', 'ui.router', 'ui.bootstrap', 'ui.bootstrap.showErrors', 'CitrusHack.controllers', 'CitrusHack.directives', 'CitrusHack.constants', 'CitrusHack.services'])
-        .run(['$rootScope', 'SCHOOLS', 'CLASS_LEVELS', 'APPLICANT_TYPES', function($rootScope, SCHOOLS, CLASS_LEVELS, APPLICANT_TYPES){
+    angular.module('CitrusHack', ['ngAnimate', 'ui.router', 'ui.bootstrap', 'ui.bootstrap.showErrors', 'duScroll', 'CitrusHack.controllers', 'CitrusHack.directives', 'CitrusHack.constants', 'CitrusHack.services'])
+        .run(['$rootScope', '$location', 'SCHOOLS', 'CLASS_LEVELS', 'APPLICANT_TYPES', function($rootScope, $location, SCHOOLS, CLASS_LEVELS, APPLICANT_TYPES){
             // Make constant accessible in partials
             $rootScope.SCHOOLS = SCHOOLS;
             $rootScope.CLASS_LEVELS = CLASS_LEVELS;
             $rootScope.APPLICANT_TYPES = APPLICANT_TYPES;
+            // Make location available in rootScope
+            $rootScope.location = $location;
         }])
         // Configure angular-show-errors to also show
         // valid form entries
