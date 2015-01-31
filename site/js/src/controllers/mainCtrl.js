c5147788ddb141a8b284b996ab3f41b8cebc4c1a
omoman <mannysanchez998@gmail.com>
2015-01-31 13:14:19 -0800

    Started contact validation and changed style

--- site/js/src/controllers/mainCtrl.js
+++ site/js/src/controllers/mainCtrl.js
@@ -5,13 +5,18 @@
      * Main controller
      */
 
-    function mainCtrl ($scope, $rootScope, $modal) {
+    function mainCtrl ($scope, $rootScope, $modal, Contact) {
         // Name of the view
         $scope.pageView = 'main-view';
         // Security breach elaboration
         $scope.securityElaborate = 'Although your project could be security related';
         // Project ideas explanation
         $scope.projectIdeasType = 'These projects would be a mix of humanitarian and utility applications'; 
+        // Bind contact
+        $scope.Contact = Contact;
+        // See if we are sending a form
+        $scope.submitting = false;
+
         if ($rootScope.successApply){
             $modal.open({
                 templateUrl: 'partials/successAppModal.html'
@@ -24,6 +29,19 @@
             // so we don't show the modal again
             $rootScope.successApply = false;
         }
+
+        // Send contact form
+        $scope.sendMessage = function () {
+            $scope.$broadcast('show-errors-check-validity');
+            if ($scope.contactForm.$valid) {
+                $scope.submitting = true;
+                // Send the from
+                Contact.sendMessage()
+                    .finally(function(){
+                        $scope.submitting = false;
+                    });
+            }
+        }
     }
 
     /**
@@ -31,6 +49,6 @@
      */
 
     angular.module('CitrusHack.controllers')
-        .controller('MainCtrl', ['$scope', '$rootScope', '$modal', mainCtrl]);
+        .controller('MainCtrl', ['$scope', '$rootScope', '$modal', 'Contact', mainCtrl]);
 
 })();
