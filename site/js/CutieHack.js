517184a74f61f23145e3b5941d9c52f8bc6e3d00
Kyle Minshall <kyleminshall@gmail.com>
2016-04-18 06:04:18 -0700

    Fixed nav drawer on mobile

--- site/js/CutieHack.js
+++ site/js/CutieHack.js
@@ -69,47 +69,6 @@
     'use strict';
 
     /**
-     * Main controller
-     */
-
-    function mainCtrl ($scope, $rootScope, $modal, Contact) {
-        // Name of the view
-        $scope.pageView = 'main-view';
-        // Security breach elaboration
-        $scope.securityElaborate = 'Although your project could be security related';
-        // Project ideas explanation
-        $scope.projectIdeasType = 'These projects would be a mix of humanitarian and utility applications'; 
-        // Bind contact
-        $scope.Contact = Contact;
-        // See if we have sent the contact form
-        $scope.sentForm = false;
-
-        // Send contact form
-        $scope.sendMessage = function () {
-            $scope.$broadcast('show-errors-check-validity');
-            if ($scope.contactForm.$valid) {
-                // Send the form
-                Contact.sendMessage()
-                    .finally(function(){
-                        $scope.sentForm = true;
-                    });
-            }
-        };
-    }
-
-    /**
-     * Register controller
-     */
-
-    angular.module('CutieHack.controllers')
-        .controller('MainCtrl', ['$scope', '$rootScope', '$modal', 'Contact', mainCtrl]);
-
-})();
-
-(function(){
-    'use strict';
-
-    /**
      * Parallax scrolling directive
      */
 
@@ -174,6 +133,47 @@
 
 })();
 
+
+(function(){
+    'use strict';
+
+    /**
+     * Main controller
+     */
+
+    function mainCtrl ($scope, $rootScope, $modal, Contact) {
+        // Name of the view
+        $scope.pageView = 'main-view';
+        // Security breach elaboration
+        $scope.securityElaborate = 'Although your project could be security related';
+        // Project ideas explanation
+        $scope.projectIdeasType = 'These projects would be a mix of humanitarian and utility applications'; 
+        // Bind contact
+        $scope.Contact = Contact;
+        // See if we have sent the contact form
+        $scope.sentForm = false;
+
+        // Send contact form
+        $scope.sendMessage = function () {
+            $scope.$broadcast('show-errors-check-validity');
+            if ($scope.contactForm.$valid) {
+                // Send the form
+                Contact.sendMessage()
+                    .finally(function(){
+                        $scope.sentForm = true;
+                    });
+            }
+        };
+    }
+
+    /**
+     * Register controller
+     */
+
+    angular.module('CutieHack.controllers')
+        .controller('MainCtrl', ['$scope', '$rootScope', '$modal', 'Contact', mainCtrl]);
+
+})();
 
 (function() {
     'use strict';
